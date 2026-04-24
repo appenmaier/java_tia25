40b702baefc3111f33f1b443ab3103c53e40f1cc
Daniel Appenmaier <daniel.appenmaier@gmail.com>
2026-04-24 09:04:56 +0200

    Add Loops and String Comparisons to Demo 1 (Java Basics)

--- src/main/java/main/D01_JavaBasics.java
+++ src/main/java/main/D01_JavaBasics.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class D01_JavaBasics {
 
    public static void main(String[] args) {
+      /* Datentypen und Datenobjekte */
       int length = "Hello World".length();
       System.out.println(length);
 
@@ -13,8 +14,18 @@ public class D01_JavaBasics {
       System.out.print("Gib bitte Deinen Namen ein: ");
       String name = scanner.next();
 
-      System.out.print("Gib bitte Dein Alter ein: ");
-      int age = scanner.nextInt();
+      int age = 0;
+      boolean loop;
+      do {
+         System.out.print("Gib bitte Dein Alter ein: ");
+         age = scanner.nextInt();
+         if (age > 0 && age < 150) {
+            loop = false;
+         } else {
+            System.out.println("ungültiger Wertebereich");
+            loop = true;
+         }
+      } while (loop);
 
       System.out.print("Gib bitte Deine Größe in Meter ein: ");
       double sizeInM = scanner.nextDouble();
@@ -25,6 +36,7 @@ public class D01_JavaBasics {
       System.out.print("Gib bitte ein, ob Du glücklich bist (true, false): ");
       boolean isHappy = scanner.nextBoolean();
 
+      /* Verzweigungen */
       // Vergleichsoperatoren: >, >=, <, <=, ==, !=
       // Logische Operatoren: &&, ||, !
       if ((gender == 'm' || gender == 'M') && age >= 18) {
@@ -48,6 +60,48 @@ public class D01_JavaBasics {
       System.out.println(sizeInM);
       System.out.println(gender);
       System.out.println(isHappy);
+
+      /* Fallunterscheidungen */
+      String genderText;
+      genderText = switch (gender) {
+         case 'm', 'M' -> "männlich";
+         case 'w', 'W' -> "weiblich";
+         case 'd', 'D' -> "divers";
+         default       -> gender + "";
+      };
+      System.out.println(genderText);
+
+      /* Schleifen */
+      int x = 1;
+      while (x <= 10) {
+         System.out.println(x);
+         x++;
+      }
+      System.out.println();
+
+      int z = 1;
+      do {
+         System.out.println(z);
+         z++;
+      } while (z <= 10);
+      System.out.println();
+
+      for (int i = 1; i <= 10; i++) {
+         System.out.println(i);
+      }
+
+      /* Vergleich von Zeichenketten */
+      String text = "Hello";
+      System.out.print("Gib bitte die Zeichenkette \"Hello\" ein: ");
+      String input = scanner.next();
+
+      if (text.equals(input)) {
+         System.out.println("Gut gemacht!");
+      } else {
+         System.out.println("Nö, das war wohl nix");
+         System.out.println(text);
+         System.out.println(input);
+      }
    }
 
 }
